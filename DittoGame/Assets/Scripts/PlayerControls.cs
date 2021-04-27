e8e5fba3e9b250df97d3acd11195e7fcefc3c0e3
cheungkayuk <cheungkayuk121@gmail.com>
2021-04-27 13:53:52 +0800

    allow flipping

--- DittoGame/Assets/Scripts/PlayerControls.cs
+++ DittoGame/Assets/Scripts/PlayerControls.cs
@@ -61,10 +61,14 @@ public class PlayerControls : Photon.MonoBehaviour
             if (Input.GetKey(KeyCode.LeftArrow))
             {
                 v.x = -speed;
+                FlipTrue();
+                photonView.RPC("FlipTrue", PhotonTargets.AllBuffered);
             }
             else if (Input.GetKey(KeyCode.RightArrow))
             {
                 v.x = speed;
+                FlipFalse();
+                photonView.RPC("FlipFalse", PhotonTargets.AllBuffered);
             }
             else
             {
